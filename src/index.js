1f41d0ac16363a447a6c29e01c61be5748904975
PulseFiction <seb-ldn@hotmail.co.uk>
2022-03-24 08:14:10 +0000

    Added more style and script functionality to task list.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-import './style.css';
-import taskList from './tasklist';
+import "./style.css";
+import taskList from "./tasklist";
 
-const addButton = document.querySelector('.sidebar__button');
+const addButton = document.querySelector(".sidebar__button");
 
-addButton.addEventListener('click', newProject)
-
-
+/*
 function newProject() {
     
     const userInput = document.querySelector('.sidebar__input')
@@ -28,10 +26,52 @@ function newProject() {
     projectListEntry.addEventListener('click', taskList)
 
 }
+*/
+
+class Project {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+class UI {
+  static newProject() {
+    const userInput = document.querySelector(".sidebar__input");
+    const projectName = userInput.value;
+
+    const projectList = document.querySelector(".sidebar__list");
+    const projectListEntry = document.createElement("li");
+
+    const errorMessage = document.querySelector(".sidebar__error");
+    
+
+    if (projectName.length <= 0) {
+      errorMessage.style.display = "block";
+      
+    } else {
+      errorMessage.style.display = "none";
+      projectListEntry.textContent = projectName;
+      projectListEntry.classList.add(projectName);
+      projectList.appendChild(projectListEntry);
+
+      const book = new Project(projectName);
+    }
+
+    projectListEntry.addEventListener("click", taskList);
+  }
+
+  static addTask() {
+     
 
 
+    
+  }
+}
 
+class Store {
+  static addBook(book) {}
+}
 
-
+addButton.addEventListener("click", UI.newProject);
 
 
--- src/tasklist.js
+++ src/tasklist.js
@@ -2,16 +2,80 @@ export default function taskList() {
   const main = document.querySelector(".main");
 
   const addButton = document.createElement("button");
+
   addButton.textContent = "Add Task";
   addButton.classList.add("main__add-button");
-
   main.appendChild(addButton);
 
-  addButton.addEventListener("click", () => {
-    const userTask = prompt("Add new task");
-    const taskDiv = document.createElement("div");
+  addButton.addEventListener("click", (e) => {
+    addButton.style.display = "none";
 
-    if (userTask.length < 1) {
+    // taskLabel
+    const taskLabel = document.createElement("label");
+    taskLabel.id = "taskName";
+    taskLabel.textContent = "Enter your task:";
+
+    //taskInput
+    const taskInput = document.createElement("input");
+    taskInput.id = "taskName";
+
+    // Submit Button
+    const submitButton = document.createElement("button");
+    submitButton.type = "submit";
+    submitButton.classList.add("main__submit-button");
+    submitButton.textContent = "Submit";
+
+    // Appends to main div
+    main.appendChild(taskLabel);
+    main.appendChild(taskInput);
+    main.appendChild(submitButton);
+
+    submitButton.addEventListener("click", () => {
+      const userTask = taskInput.value;
+      
+      const taskDiv = document.createElement("div");
+
+      // Priority Button
+      const priorityButton = document.createElement('button');
+      priorityButton.classList.add('main__task-list__priority-button');
+      // Task description
+      const taskDescription = document.createElement('p');
+      // Remove Button
+      const removeButton = document.createElement('button');
+      removeButton.textContent = 'x'
+      removeButton.classList.add('main__task-list__remove-button')
+
+      // Alert 
+      const submitAlert = document.createElement('p')
+      setTimeout(() => submitAlert.remove(), 2000)
+      // hr that appears under each task
+      const line = document.createElement('hr')
+
+
+      if (userTask.length < 1) {
+        main.insertBefore(submitAlert, taskInput)
+        submitAlert.textContent = 'Please enter a task!'
+        submitAlert.style.color = 'var(--clr-red)';
+      } else {
+        taskDescription.textContent = userTask;
+        taskDiv.classList.add("main__task-list");
+
+
+        // append to taskList div
+        taskDiv.appendChild(priorityButton);
+        taskDiv.appendChild(taskDescription);
+        taskDiv.appendChild(removeButton);
+
+        // appends to main
+        main.appendChild(taskDiv);
+        main.append(line)
+      }
+    });
+  });
+}
+
+/*   if (userTask.length < 1) {
+  const taskDiv = document.createElement("div");
       alert("Please enter a task.");
     } else {
       taskDiv.textContent = userTask;
@@ -19,4 +83,4 @@ export default function taskList() {
       main.appendChild(taskDiv);
     }
   });
-}
+  */

5a7030593dcaadd812694b7cd9c4d80054e049aa
PulseFiction <seb-ldn@hotmail.co.uk>
2022-06-12 14:10:17 +0100

    added new icon for list projects, styled list too.

--- src/createProject.js
+++ src/createProject.js
@@ -1,14 +1,9 @@
-
+import Arrow from "./images/arrow.png";
 
 export function addProject() {
     
     const button = document.querySelector('.add-project__button');
     const projectDiv = document.querySelector('.add-project__div');
-    
-    
-    
-   
-    
     
     button.addEventListener('click', () => {
         const titleInput = document.createElement('input');
@@ -27,14 +22,39 @@ export function addProject() {
         
         submit.addEventListener('click', () => {
             const projectsList = document.querySelector('.sidebar__projects__ul');
-            
+            const projectMain = document.querySelector('.projects__div');
             if (titleInput.value <= 0) {
                 titleInput.classList.add('add-project__input-error')
                 titleInput.placeholder = 'Name not entered!'
             } else {
-                const project = document.createElement('h3');
-                project.textContent = titleInput.value;
-                projectsList.appendChild(project);
+                const projectListItem = document.createElement('li');
+                const arrowIcon = new Image();
+                arrowIcon.src = Arrow;
+                projectListItem.appendChild(arrowIcon);
+                
+                const projectListName = document.createElement('h3');
+                const projectBoxName = document.createElement('h3');
+                const projectDiv = document.createElement('div');
+                projectDiv.classList.add('project__container')
+                const todoDiv = document.createElement('div');
+                
+                projectListName.textContent = titleInput.value;
+                projectBoxName.textContent = titleInput.value;
+
+
+                projectListItem.appendChild(projectListName)
+                projectDiv.appendChild(projectBoxName);
+                projectDiv.appendChild(todoDiv);
+
+                projectsList.appendChild(projectListItem);
+                projectMain.appendChild(projectDiv)
+            
+
+
+
+
+
+
                 button.style.display = 'block';
                 submit.style.display = 'none';
                 titleInput.style.display = 'none';
